c0e69174493b0bd174d869edf1c414df75f80a1f
Corgi <amoalexandru5@gmail.com>
2026-04-17 04:53:35 +0300

    feat(alo): widgets-core barrel export

---
+++ src/components/widgets-core/index.js
@@ -0,0 +1,5 @@
+export { default as MatrixGrid, emptyMatrixValue, readMatrixValue } from './MatrixGrid';
+export { default as MatrixDisplay } from './MatrixDisplay';
+export { default as VectorInput } from './VectorInput';
+export { default as StepPlayer } from './StepPlayer';
+export { default as FeatTray } from './FeatTray';
